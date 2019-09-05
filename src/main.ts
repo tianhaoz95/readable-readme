6acb58f546ffebb8eb19eddf437ed1e7a889ecea
tianhaoz95 <tianhaoz@umich.edu>
2019-09-04 21:40:19 -0700

    dev: add tslint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import * as core from '@actions/core';
+import * as core from "@actions/core";
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
+    const myInput = core.getInput("myInput");
     core.debug(`Hello ${myInput}`);
   } catch (error) {
     core.setFailed(error.message);
