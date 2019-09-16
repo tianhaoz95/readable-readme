8821362626c550f770f1a56842d519057d2d7be6
Tianhao Zhou <tianhaoz@umich.edu>
2019-09-16 15:12:38 -0700

    dev: add snippet getter (#31)
    * dev: snippet getter and tests

--- __tests__/report.test.ts
+++ __tests__/report.test.ts
@@ -50,4 +50,35 @@ describe("Report test suite", () => {
       report.getTeportIssueTitle();
     }).not.toThrow();
   });
+
+  it("snippet getter no crash", () => {
+    expect(() => {
+      const testText = "This is a test and hope it works lol!";
+      report.getSnippet(5, 2, 1, testText);
+    }).not.toThrow();
+  });
+
+  it("snippet getter basic", () => {
+    const testText = "This is a test and hope it works lol!";
+    expect(
+      report.getSnippet(10, 4, 4, testText),
+    ).toEqual({
+      fullSnippet: "s a test and",
+      highlight: "test",
+      leftContext: "s a ",
+      rightContext: " and",
+    });
+  });
+
+  it("snippet getter out of bound", () => {
+    const testText = "This is a test and hope it works lol!";
+    expect(
+      report.getSnippet(10, 4, 100, testText),
+    ).toEqual({
+      fullSnippet: "This is a test and hope it works lol!",
+      highlight: "test",
+      leftContext: "This is a ",
+      rightContext: " and hope it works lol!",
+    });
+  });
 });
