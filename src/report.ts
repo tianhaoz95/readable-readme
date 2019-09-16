c0f41983d82506d0f387c3f8faedcb2198053f6a
tianhaoz95 <tianhaoz@umich.edu>
2019-09-15 20:29:41 -0700

    dev: snippet getter and tests

--- src/report.ts
+++ src/report.ts
@@ -29,3 +29,34 @@ export function getTeportIssueTitle(): string {
   // ientifiers, timestamp and commit sha.
   return "placeholder titile";
 }
+
+/**
+ * Snippet getter will wrap a flexible size of context
+ * around our suggestion highlight.
+ * @param index The starting index of the suggestion highlight
+ * @param offset The offset of the suggestion highlight
+ * @param range The range of the context
+ * @param fullText The full text to get snippet from
+ * @returns The snippet
+ */
+export function getSnippet(
+  index: number,
+  offset: number,
+  range: number,
+  fullText: string,
+) {
+  const textSize = fullText.length;
+  const startIndex = Math.max(0, index - range);
+  const endIndex = Math.min(textSize, index + offset + range);
+  const highlight = fullText.substring(index, index + offset);
+  const leftContext = fullText.substring(startIndex, index);
+  const rightContext = fullText.substring(index + offset, endIndex);
+  const fullSnippet = leftContext + highlight + rightContext;
+  return {
+    fullSnippet,
+    highlight,
+    leftContext,
+    rightContext,
+  };
+}
+// TODO(tianhaoz95): add more tests for edge cases
