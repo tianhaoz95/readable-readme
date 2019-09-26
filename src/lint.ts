a0ecae7251778d783557c5a16d0a3b9c9cc08eb7
Tianhao Zhou <tianhaoz@umich.edu>
2019-09-25 23:03:27 -0700

    dev: file link (#120)
    * dev: add wildcard matcher
    
    * dev: add file link

--- src/lint.ts
+++ src/lint.ts
@@ -1,4 +1,6 @@
 import * as core from "@actions/core";
+import micromatch from "micromatch";
+import * as path from "path";
 import en from "./en";
 import * as octo from "./octo";
 import * as report from "./report";
@@ -8,16 +10,20 @@ export async function lintWorkspace() {
   try {
     const verbose = core.getInput("verbose");
     core.debug(`Verbose level: ${verbose}`);
-    const workspaceDir = util.getGitHubWorkspace() + "/horrible";
-    const workspaceFiles = util.listFiles(workspaceDir);
+    const workspaceDir = util.getGitHubWorkspace();
+    const rawWorkspaceFiles = util.listFiles(workspaceDir);
+    const workspaceFiles = micromatch(rawWorkspaceFiles, ["!**/node_modules/**"]);
     const reportsMetadata = new Array();
     for (const workspaceFile of workspaceFiles) {
       if (util.isReadmeFilename(workspaceFile)) {
         const readmeFileContent = util.readFileContent(workspaceFile);
+        const relativePath = path.relative(workspaceDir, workspaceFile);
+        core.debug("Scanning " + workspaceFile + ", relative path: " + relativePath + "...");
         const reportEntry = {
           en: null,
           fileContent: readmeFileContent,
           filename: workspaceFile,
+          relativePath,
         };
         reportEntry.en = en(readmeFileContent);
         reportsMetadata.push(reportEntry);
@@ -26,9 +32,17 @@ export async function lintWorkspace() {
     let finalReport = "final report";
     for (const reportMetadata of reportsMetadata) {
       const reportEntry = report.composeReportMetadataToParagraph(reportMetadata);
-      finalReport += "\n\n";
-      finalReport += reportEntry;
-      finalReport += "\n\n";
+      // TODO(tianhaoz95): let the compose take the responsibility of decisiding
+      // whether the report for certain file should be generated.
+      // TODO(tianhaoz95): add a test for no suggestion composing
+      // (shouldn't generate anything).
+      if (reportMetadata.en.length > 0) {
+        finalReport += "\n\n";
+        finalReport += reportEntry;
+        finalReport += "\n\n";
+      } else {
+        core.debug("Skipping because no suggestion is generated");
+      }
     }
     const reportTitle = report.getTeportIssueTitle();
     await octo.postGitHubIssue(reportTitle, finalReport);
--- template/langSuggestion.md
+++ template/langSuggestion.md
@@ -1,6 +1,8 @@
 <details>
   <summary>Suggestion for snippet @ index:{{{index}}} & offset:{{{offset}}}</summary>
 
+[go to file](https://github.com/{{{owner}}}/{{{repo}}}/blob/{{{ref}}}/{{{path}}})
+
 ...
 
 {{{snippet}}}
