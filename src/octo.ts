758cbb55fde1cc3d13bc11a143d91bd62fac52cc
Tianhao Zhou <tianhaoz@umich.edu>
2020-07-24 17:13:18 -0700

    fix: syntax

--- src/octo.ts
+++ src/octo.ts
@@ -7,7 +7,7 @@ function getOctokit() {
   if (process.env.TEST_GITHUB_TOKEN) {
     token = process.env.TEST_GITHUB_TOKEN;
   }
-  const kit = new github.getOctokit(token);
+  const kit = github.getOctokit(token);
   return kit;
 }
 
