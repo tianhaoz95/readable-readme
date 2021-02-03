d6a2d5cdc4e1974da6b09f5143fa62812d92a8c7
undefined <tianhaoz@umich.edu>
2021-02-03 02:35:46 +0000

    chore(js-action-bot): update node modules

---
+++ node_modules/shiki/src/__tests__/simple.test.ts
@@ -0,0 +1,9 @@
+import { getHighlighter } from '../index'
+
+test('Nord highlighter highlights simple JavaScript', async () => {
+  const highlighter = await getHighlighter({
+    theme: 'nord'
+  })
+  const out = highlighter.codeToHtml(`console.log('shiki');`, 'js')
+  expect(out).toMatchSnapshot()
+})
